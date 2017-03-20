0daa6edf6f110d97e0a4e9342077c76a4d8949fa
Idaml <idaml@sela.co.il>
2017-03-20 13:47:07 +0200

    Created new branch from master

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, CanActivate } from '@angular/router';
 import { MaterialModule } from '@angular/material';
@@ -8,7 +8,7 @@ import { DataTableModule } from 'angular2-datatable';
 import { DynamicFormsCoreModule } from "@ng2-dynamic-forms/core";
 import { DynamicFormsBootstrapUIModule } from "@ng2-dynamic-forms/ui-bootstrap";
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 import { MainAppComponent } from './mainApp/mainApp.component';
 import { LoginRegisterComponent } from './loginRegistration/loginRegister.component';
 import { LogoutComponent } from './loginRegistration/logout.component';
@@ -26,48 +26,49 @@ import { CanDeactivateDiagnosisFormGuard } from "./shared/services/canDeactivate
 
 
 @NgModule({
-  imports:      [ BrowserModule,
-                  FormsModule,
-                  HttpModule,
-                  DataTableModule,
-                  MaterialModule.forRoot(),
-                  ReactiveFormsModule,
-                  DynamicFormsCoreModule.forRoot(),
-                  DynamicFormsBootstrapUIModule,
-                  RouterModule.forRoot([
-                    {path: 'login', component: LoginRegisterComponent},
-                    {path: 'login/:form', component: LoginRegisterComponent},
-                    {path: 'register', redirectTo: 'login/1', pathMatch:'full'},
-                    {path: 'passwordrecovery', redirectTo: 'login/2', pathMatch:'full'},
-                    {path: 'logout/:id', component: LogoutComponent},
-                    {path: 'patientInfo', component: PatientInfoComponent, canActivate : [CanActivateOAuthGuard]},
-                    {path: 'patientEdit/:id', component: PatientEditInfoComponent, canActivate : [CanActivateOAuthGuard]},
-                    {path: 'patientDiagnosisDetails/:id',component:PatientDiagnosisDetailsComponent,canActivate:[CanActivateOAuthGuard],canDeactivate:[CanDeactivateDiagnosisFormGuard]},
-                    {path: 'findPatient', component: FindPatientComponent, canActivate : [CanActivateOAuthGuard]},
-                    // {path: 'userManagment', component: UnderConstructionComponent, canActivate : [CanActivateOAuthGuard]},
-                    // {path: 'research', component: UnderConstructionComponent, canActivate : [CanActivateOAuthGuard]},
-                    // {path: 'personalInfo', component: UnderConstructionComponent, canActivate : [CanActivateOAuthGuard]},
-                    // {path: '', redirectTo: 'findPatient', pathMatch:'full', canActivate : [CanActivateOAuthGuard]},
-                    // {path: '**', redirectTo: 'findPatient', pathMatch:'full', canActivate : [CanActivateOAuthGuard]},
-                    {path: '', redirectTo: 'login', pathMatch:'full'},
-                    {path: '**', redirectTo: 'login', pathMatch:'full'}
-                  ])
-                ],
-  declarations: [ AppComponent,
-                  MainAppComponent,
-                  LoginRegisterComponent,
-                  LogoutComponent,
-                  EqualValidator,
-                  PatientInfoComponent,
-                  PatientEditInfoComponent,
-                  PatientDiagnosisDetailsComponent,
-                  FindPatientComponent,
-                  TabComponent,
-                  TabsComponent,
-                  EnumToOptionsFilter,
-                  DataFilterPipe
-                ],
-  providers:    [CanActivateOAuthGuard,CanDeactivateDiagnosisFormGuard],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule,
+    FormsModule,
+    HttpModule,
+    DataTableModule,
+    MaterialModule.forRoot(),
+    ReactiveFormsModule,
+    DynamicFormsCoreModule.forRoot(),
+    DynamicFormsBootstrapUIModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginRegisterComponent },
+      { path: 'login/:form', component: LoginRegisterComponent },
+      { path: 'register', redirectTo: 'login/1', pathMatch: 'full' },
+      { path: 'passwordrecovery', redirectTo: 'login/2', pathMatch: 'full' },
+      { path: 'logout/:id', component: LogoutComponent },
+      { path: 'patientInfo', component: PatientInfoComponent, canActivate: [CanActivateOAuthGuard] },
+      { path: 'patientEdit/:id', component: PatientEditInfoComponent, canActivate: [CanActivateOAuthGuard] },
+      { path: 'patientDiagnosisDetails/:id', component: PatientDiagnosisDetailsComponent, canActivate: [CanActivateOAuthGuard], canDeactivate: [CanDeactivateDiagnosisFormGuard] },
+      { path: 'findPatient', component: FindPatientComponent, canActivate: [CanActivateOAuthGuard] },
+      // {path: 'userManagment', component: UnderConstructionComponent, canActivate : [CanActivateOAuthGuard]},
+      // {path: 'research', component: UnderConstructionComponent, canActivate : [CanActivateOAuthGuard]},
+      // {path: 'personalInfo', component: UnderConstructionComponent, canActivate : [CanActivateOAuthGuard]},
+      // {path: '', redirectTo: 'findPatient', pathMatch:'full', canActivate : [CanActivateOAuthGuard]},
+      // {path: '**', redirectTo: 'findPatient', pathMatch:'full', canActivate : [CanActivateOAuthGuard]},
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: '**', redirectTo: 'login', pathMatch: 'full' }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    MainAppComponent,
+    LoginRegisterComponent,
+    LogoutComponent,
+    EqualValidator,
+    PatientInfoComponent,
+    PatientEditInfoComponent,
+    PatientDiagnosisDetailsComponent,
+    FindPatientComponent,
+    TabComponent,
+    TabsComponent,
+    EnumToOptionsFilter,
+    DataFilterPipe
+  ],
+  providers: [CanActivateOAuthGuard, CanDeactivateDiagnosisFormGuard],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
